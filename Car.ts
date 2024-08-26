5100478af5d7c23cde03ab17c2d63ecc9054d041
Onyinyechi14 <giftjulius070@gmail.com>
2024-08-26 11:08:15 +0100

    initial commit

---
+++ Car.ts
@@ -0,0 +1,16 @@
+export class Car {
+    make: string;
+    model: string;
+    year: number;
+  
+    constructor(make: string, model: string, year: number) {
+      this.make = make;
+      this.model = model;
+      this.year = year;
+    }
+  
+    start(): void {
+      console.log("Car engine started");
+    }
+  }
+  
---
+++ Index.ts
@@ -0,0 +1,4 @@
+import { Car } from './Car';
+
+const myCar = new Car("Toyota", "Corolla", 2020);
+myCar.start(); 
